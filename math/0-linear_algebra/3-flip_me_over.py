e55219efbbecd8ad0f0ba8eead01ab7682c248da
Melkin Mosquera <melanmosta@gmail.com>
2020-10-16 14:59:09 -0500

    transpose of a matrix

---
+++ math/0-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# returns the transpose of a 2D matrix
+
+def matrix_transpose(matrix):
+    """Transpose of a matrix"""
+    transpose = []
+    for i in range(len(matrix[0])):
+        transpose.append([])
+        for j in range(len(matrix)):
+            transpose[i].append(matrix[j][i])
+
+    return transpose
